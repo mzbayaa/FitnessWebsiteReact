9c43df2b328b5646971e45e25c639ba4a8135fb8
Mohamad Bayaa <mbaya053@uottawa.ca>
2023-06-11 18:28:29 -0400

    cleaned up code

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -24,17 +24,6 @@ const Footer = () => {
               <a href="/contact-us">Contact Us</a>
             </li>
           </ul>
-          <div className="footer-social-icons">
-            <a href="https://facebook.com">
-              <i className="fa fa-facebook"></i>
-            </a>
-            <a href="https://twitter.com">
-              <i className="fa fa-twitter"></i>
-            </a>
-            <a href="https://instagram.com">
-              <i className="fa fa-instagram"></i>
-            </a>
-          </div>
         </div>
       </div>
       <div className="footer-bottom">
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,11 +3,7 @@ import { Link, useNavigate } from "react-router-dom";
 import { Button } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./Navbar.css";
-import { List } from "react-bootstrap-icons";
 
-//i class name click is for hamburger
-//handleClick swaps hamberger between clicks 20min
-//useNavigate istead of link (Ahmad)
 function Navbar() {
   const [click, setClick] = useState(false);
   const handleClick = () => setClick(!click);
@@ -22,7 +18,7 @@ function Navbar() {
     }
   };
 
-  //fixes bug where book now button shows up after refresh when window width is not full
+  //bug with button in navbar
   useEffect(() => {
     showButton();
   }, []);
@@ -33,24 +29,19 @@ function Navbar() {
     <>
       <nav className="navbar">
         <div className="navbar-container">
-          {/* logo */}
-          {/* onClick added so that clicking logo closes the hamburger */}
           <Link to="/" className="navbar-logo" onClick={closeMobileMenu}>
             HABIBI MUSCLE CLUB
           </Link>
           <div className="menu-icon" onClick={handleClick}>
             <i className="bi bi-list" />
           </div>
-          {/* clicking button will make it disapear after selection */}
           <ul className={click ? "nav-menu active" : "nav-menu"}>
             <li className="nav-item">
-              {/* Home button */}
               <Link to="/" className="nav-links" onClick={closeMobileMenu}>
                 Home
               </Link>
             </li>
             <li className="nav-item">
-              {/* Services button */}
               <Link
                 to="/services"
                 className="nav-links"
@@ -60,7 +51,6 @@ function Navbar() {
               </Link>
             </li>
             <li className="nav-item">
-              {/* Our Trainers button */}
               <Link
                 to="/our-trainers"
                 className="nav-links"
@@ -70,7 +60,6 @@ function Navbar() {
               </Link>
             </li>
             <li className="nav-item">
-              {/* Contact Us button */}
               <Link
                 to="/contact-us"
                 className="nav-links"
@@ -80,7 +69,6 @@ function Navbar() {
               </Link>
             </li>
             <li>
-              {/* book now button */}
               {button && (
                 <Button
                   className="btn btn-dark btn-outline-light"
