c6247c2830a45db34d6bb189934330322a76b322
Mohamad Bayaa <mbaya053@uottawa.ca>
2023-06-11 22:25:20 -0400

    final touches

--- src/components/pages/Confirm Booking.js
+++ src/components/pages/Confirm Booking.js
@@ -7,6 +7,8 @@ import "bootstrap/dist/css/bootstrap.min.css";
 
 export default function Information() {
   const navigate = useNavigate();
+  const bookingNumber = Math.floor(Math.random() * 1000000);
+
   return (
     <div className="page">
       <h1 className="header">Booking Confirmed!</h1>
@@ -14,6 +16,7 @@ export default function Information() {
         We have succecufully received and confirmed your reservation request.{" "}
       </p>
       <p>An email confirming your reservation will be sent to you shortly </p>
+      <p>Booking Number: #{bookingNumber} </p>
       <div className="button">
         <Button
           className="btn btn-success"
